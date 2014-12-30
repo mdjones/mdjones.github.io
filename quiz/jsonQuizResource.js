6d53e8e4af226004340b4e4a29674f8214fef815
mdjones <mdjones66@gmail.com>
2014-12-30 15:05:30 -0500

    Seperate Dirs

---
+++ quiz/jsonQuizResource.js
@@ -0,0 +1,169 @@
+quizzes = [
+    {
+    "id": "Assertiveness",
+    "name": "Assertiveness",
+    "questions": [
+        {
+            "question": "I tend to yell at people when I don't get my way.",
+            "variable": "directness",
+            "sign": "positive",
+            "id": 1
+        },
+        {
+            "question": "It annoys me that others get their way and I just miss out.",
+            "variable": "directness",
+            "sign": "negative",
+            "id": 2
+        },
+        {
+            "question": "I often have others tell me that I'm pushy.",
+            "variable": "directness",
+            "sign": "positive",
+            "id": 3
+        },
+        {
+            "question": "I generally feel satisfied that I can ask for what I want.",
+            "variable": "directness",
+            "sign": "positive",
+            "id": 4
+        },
+        {
+            "question": "I find clever ways to subtly get back at people, after I get angry that I'm always doing what they want.",
+            "variable": "directness",
+            "sign": "negative",
+            "id": 5
+        },
+        {
+            "question": "I often say 'I don't know, what do you want to do?'",
+            "variable": "directness",
+            "sign": "negative",
+            "id": 6
+        },
+        {
+            "question": "I ask for help when I need it.",
+            "variable": "directness",
+            "sign": "positive",
+            "id": 7
+        },
+        {
+            "question": "I use the 'silent treatment' when I resent what someone else has said or done.",
+            "variable": "directness",
+            "sign": "negative",
+            "id": 8
+        },
+        {
+            "question": "I have been known to lose my temper and tell people off.",
+            "variable": "caring",
+            "sign": "negative",
+            "id": 9
+        },
+        {
+            "question": "I feel like people take advantage of me.",
+            "variable": "caring",
+            "sign": "positive",
+            "id": 10
+        },
+        {
+            "question": "I am able to say \"no\" when I don't want to do something.",
+            "variable": "directness",
+            "sign": "positive",
+            "id": 11
+        },
+        {
+            "question": "I sometimes make other people look or feel stupid, small, or afraid.",
+            "variable": "caring",
+            "sign": "negative",
+            "id": 12
+        },
+        {
+            "question": "When an argument is over, I replay the situation in my head, thinking of ways I can get some 'quiet revenge.'",
+            "variable": "directness",
+            "sign": "negative",
+            "id": 13
+        },
+        {
+            "question": "I am able to generally speak up when others start to plan things I don't want to do.",
+            "variable": "directness",
+            "sign": "positive",
+            "id": 14
+        },
+        {
+            "question": "I tend to just go along with what everyone else wants, instead of stating my own desires.",
+            "variable": "directness",
+            "sign": "negative",
+            "id": 15
+        },
+        {
+            "question": "When someone bugs me, I hope they notice that I get a little more difficult to deal with.",
+            "variable": "directness",
+            "sign": "negative",
+            "id": 16
+        },
+        {
+            "question": "It makes me feel good to get back at someone who has disrespected me.",
+            "variable": "caring",
+            "sign": "negative",
+            "id": 17
+        },
+        {
+            "question": "I wish I could speak up more for my own opinions.",
+            "variable": "caring",
+            "sign": "positive",
+            "id": 18
+        },
+        {
+            "question": "I resent it when someone gets all the credit for something I helped on.",
+            "variable": "caring",
+            "sign": "negative",
+            "id": 19
+        },
+        {
+            "question": "I prefer to tell people directly when they're doing something I don't like.",
+            "variable": "caring",
+            "sign": "positive",
+            "id": 20
+        },
+        {
+            "question": "If someone can't deal with my directness, too bad.",
+            "variable": "caring",
+            "sign": "negative",
+            "id": 21
+        },
+        {
+            "question": "I really don't want to hurt anyone's feelings.",
+            "variable": "caring",
+            "sign": "positive",
+            "id": 22
+        },
+        {
+            "question": "I don't care if I hurt someone's feelings.",
+            "variable": "caring",
+            "sign": "negative",
+            "id": 23
+        }
+    ]},
+
+        {
+            "id": "Short",
+            "name": "Short",
+            "questions": [
+                {
+                    "question": "I tend to yell at people when I don't get my way.",
+                    "variable": "directness",
+                    "sign": "positive",
+                    "id": 1
+                },
+                {
+                    "question": "It annoys me that others get their way and I just miss out.",
+                    "variable": "directness",
+                    "sign": "negative",
+                    "id": 2
+                },
+                 {
+                     "question": "I don't care if I hurt someone's feelings.",
+                     "variable": "caring",
+                     "sign": "negative",
+                     "id": 23
+                 }
+        ]}
+]
---
+++ quiz/quizResource.js
@@ -0,0 +1,32 @@
+retrieveQuiz = function(quizID) {
+ console.log(quizzes)
+ var rQuiz
+    angular.forEach(quizzes, function(quiz){
+         if(quizID === quiz.id){
+            rQuiz = quiz
+         }
+     });
+     return rQuiz
+}
+
+retrieveQuestion = function(quiz, questionID) {
+    console.log('Looking for ' + questionID + " in ")
+    console.log(quiz)
+    var rQuestion
+    for (var i in quiz.questions){
+        question = quiz.questions[i]
+        if(questionID === question.id){
+             return question
+        }
+    }
+
+     return undefined
+}
+
+retrieveQuizzes = function() {
+    return quizzes
+}
+
+retrieveValidScores = function() {
+    return [1,2,3,4,5]
+}
